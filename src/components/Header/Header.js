76143d2b3ee5023076561b3ed5f8d6702067b9e3
Denis Borisov <borisovdenis-kb@yandex.ru>
2019-07-06 18:30:15 +0400

    Added component Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+export function Header(props) {
+  return (
+    <div className="header">
+      <div className="header__logo">Sfxdx Todo</div>
+    </div>
+  );
+}
